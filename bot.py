f6a78a495a1d531a4346b17818d95ed25eda2b1f
Parth Shah <helloparthshah@gmail.com>
2022-03-14 22:53:37 -0700

    added couse searching functionality

--- bot.py
+++ bot.py
@@ -32,8 +32,8 @@ print(c)
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 YT_KEY = os.getenv('YT_KEY')
-REMOVE_BG_KEY=os.getenv('REMOVE_BG_KEY')
-OPEN_WEATHER_KEY=os.getenv('OPEN_WEATHER_KEY')
+REMOVE_BG_KEY = os.getenv('REMOVE_BG_KEY')
+OPEN_WEATHER_KEY = os.getenv('OPEN_WEATHER_KEY')
 global_volume = 1.0
 
 bot = Client(intents=Intents.default())
@@ -311,6 +311,7 @@ async def remove_bg(ctx=SlashContext, *, user: discord.Member):
             out.write(response.content)
         await ctx.send(file=discord.File('temp.png'))
 
+
 @slash.slash(name="weather", description="Get the weather")
 async def weather(ctx=SlashContext, *, city: str):
     response = requests.get(
@@ -321,12 +322,14 @@ async def weather(ctx=SlashContext, *, city: str):
     embed.add_field(name="City", value=city, inline=False)
     embed.add_field(name="Temperature", value=str(
         round(data['main']['temp']-273.15))+"°C", inline=False)
-    embed.add_field(name="Description", value=data['weather'][0]['description'], inline=False)
+    embed.add_field(name="Description",
+                    value=data['weather'][0]['description'], inline=False)
     await ctx.send(embed=embed)
+
 
 @slash.slash(name="remind_me", description="Remind you about something after a certain time")
 async def remind_me(ctx=SlashContext, *, time: str, message: str):
-    embed=discord.Embed(title="Reminder", color=0x00ff00)
+    embed = discord.Embed(title="Reminder", color=0x00ff00)
     embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
     embed.add_field(name="Message", value=message, inline=False)
     embed.add_field(name="Time", value=time, inline=False)
@@ -335,21 +338,30 @@ async def remind_me(ctx=SlashContext, *, time: str, message: str):
     await ctx.send(ctx.author.mention)
     await ctx.send(message)
 
+
 @slash.slash(name="course", description="Get the course information")
 async def course(ctx=SlashContext, *, course: str):
     # https://mydegree.ucdavis.edu/responsiveDashboard/api/course-link?discipline=ECS&number=160
-    response = requests.get(
-        'https://mydegree.ucdavis.edu/responsiveDashboard/api/course-link?discipline='+course.split(' ')[0]+'&number='+course.split(' ')[1],
-        headers={'Cookie':'NAME=Parth%20Ninad%20Shah; REFRESH_TOKEN=Bearer+eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MTY4MjkxNzEiLCJpbnRlcm5hbElkIjoiOTE2ODI5MTcxIiwidXNlckNsYXNzIjoiU1RVIiwiYXBwTmFtZSI6ImRlZ3JlZXdvcmtzIiwibmFtZSI6IlNoYWgsIFBhcnRoIE5pbmFkIiwiZXhwaXJlSW5jcmVtZW50U2Vjb25kcyI6NTk5OTQwLCJleHAiOjE2NDc5MTk4MDksImFsdElkIjoicGFydGgxMjMiLCJpYXQiOjE2NDczMTk4NjksImp0aSI6ImNlNzA1N2VmLWI3ZmItNDEzMy05YTM0LTM0MDlhZDA2NzkyZSJ9.RtaH12n9SntJg24SdnbvpSeTrUrgs1z0-SBUC0MeUPA; X-AUTH-TOKEN=Bearer+eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MTY4MjkxNzEiLCJpbnRlcm5hbElkIjoiOTE2ODI5MTcxIiwidXNlckNsYXNzIjoiU1RVIiwiYXBwTmFtZSI6ImRlZ3JlZXdvcmtzIiwicm9sZXMiOlsiU0RBVURJVCIsIlNEQVVEUkVWIiwiU0RXSEFUSUYiLCJOT1JFRkVSIiwiU0RXSUZISVMiLCJTRFdPUktTIiwiU0RYTUwzMyIsIlNEQVVEUlVOIiwiU0RYTUwzMSIsIlNEQVVEUERGIiwiU0RMT0tBSEQiLCJTRFdFQjMxIiwiU0RXRUIzMyIsIlNEU1RVTUUiLCJTRFdFQjM2Il0sIm5hbWUiOiJTaGFoLCBQYXJ0aCBOaW5hZCIsImRlcGFydG1lbnRzIjpbXSwiZXhwIjoxNjQ3OTIwMDA3LCJhbHRJZCI6InBhcnRoMTIzIiwiaWF0IjoxNjQ3MzIwMDY3LCJqdGkiOiI2NjdmODU1Zi01NjVlLTQ3YWMtYjYwMy0wZGQwMjMyMmI2ZTcifQ.pX4LNCbTrPoJDHWVlKcWuwo23fuHV9oPEH_puToag4E; JSESSIONID=A2F4666A9E4A939FDA4613C9F0C8EF6B; _hjSessionUser_648798=eyJpZCI6ImExMzk0ODA1LTY4MmQtNTdhMy1iMDdhLTgwMjM0NTM1YTJkOCIsImNyZWF0ZWQiOjE2Mzg1NzE4MTk0MzksImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_653936=eyJpZCI6ImUxMDcyNWY3LTdlMDgtNWY0MC1iMjRmLTQxMTE3NzdkOTE3YiIsImNyZWF0ZWQiOjE2Mzg3NzI2OTU2ODUsImV4aXN0aW5nIjp0cnVlfQ==; __utmc=162187235; _hjSessionUser_648769=eyJpZCI6IjkzOWJmYmI5LTkxMjItNTc2Yi1hM2YzLTI0ZTE1YzliZDJkOCIsImNyZWF0ZWQiOjE2Mzk2MjYxMzAwNTksImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_656324=eyJpZCI6ImYyYzgxYzJmLWI2ZjQtNTFiMC1hMzk0LTJhYTM3MWVhMjA0ZiIsImNyZWF0ZWQiOjE2Mzk2Mjg2OTMwNTQsImV4aXN0aW5nIjp0cnVlfQ==; nmstat=aed74523-ab5d-f360-8392-3689cadc204e; citrix_ns_id=AAE7gX3UYTszlOcCAAAAADs9GSgA5c91NSPIO5hox76dPDDtnsvKaZO-SBVZEOITOw==B4HUYQ==XbODb_NCGQLnpHv_RJ5o2oh0K7A=; citrix_ns_id_.ucdavis.edu_%2F_wat=AAAAAAWOa83Xu667afSVnlKdioV6kBJzDq_SaGolWna8d5abXybuc354qBwMBtGXOPgmQjIt4vEs_WL-P1AAR44Q0JTo&AAAAAAUEUi_GkPTPKKW5aWHCz0JMcVLaM0-KqpSt8NK5AnaTux6vH-5f3XNzidGvbxG49UaxKQJ_Vd55R9DmHm_oxkb8RZ2iQc-B_D4VeGFYxrKnKg==&AAAAAAV9ZN2kjRuOWE1I1x2qwPWGgXE1lWVeTMtJP4rJwIF4eLpbB_YTJigwGCU3Z0xp662cUYMTTruU4Zag0lMUV9lf6rtudtCIVRpifwEsWa4v5g==&; SAP_CASAUTH=4D99A2C728D939B6A652E6CB7859C2BD43639A19F6CFCDD5FA2C60ADA08C02A1550FF5D61587732DEE15F2055B04E4513605DA27A66E9B974A4ECAF417DF27EA180B99A3164CF05F1BA3EBCA8FA0BAD92A2D7BA3EDEEDB3043FC6564032A3AF2; __utmz=162187235.1646345630.14.7.utmcsr=cas.ucdavis.edu|utmccn=(referral)|utmcmd=referral|utmcct=/; _fbp=fb.1.1646348587255.953116442; __utma=162187235.1828723012.1638322952.1646345630.1646349181.15; _ga=GA1.2.1828723012.1638322952; _ga_YQ20RZQYKR=GS1.1.1646354327.2.0.1646354327.60; _gid=GA1.2.273393137.1647237034; _hjSession_648798=eyJpZCI6IjliYTc2YTlhLTM1NzUtNGIxNi04N2Q0LTZiNmZlNjRlYzAzZCIsImNyZWF0ZWQiOjE2NDczMTk0MTg1NjIsImluU2FtcGxlIjp0cnVlfQ==; MYUCDAVIS_LANDING_IMAGE=bikesandtrees%2Ejpg%2Cblossoms%2Ejpg%2Cbunitransblur%2Ejpg%2Cccinterior%2Ejpg%2Ccpool%2Ejpg%2Ccwolfskill2%2Ejpg%2Cdeathblossom%2Ejpg%2Cdkayaks%2Ejpg%2Cdorms2%2Ejpg%2Cegret%2Ejpg%2Cflag2%2Ejpg%2CginsengMrak%2Ejpg%2Cgoags%2Ejpg%2Clibrary%2Ejpg%2Corchard%2Ejpg%2Cpallette%2Ejpg%2Cpcircle%2Ejpg%2Cplantsciences%2Ejpg%2Csciencewall%2Ejpg%2Csilo2%2Ejpg%2Cspokes%2Ejpg%2CstudentFarmPoppy%2EJPG%2Cterceroquad%2Ejpg; _gcl_au=1.1.1866842315.1647319836; _hjSession_653936=eyJpZCI6IjA4ZWMyNGQxLWZlYjItNDMxYS1hNTU0LWE1N2ZiYjg4Yjg0MCIsImNyZWF0ZWQiOjE2NDczMTk4MzYyNDksImluU2FtcGxlIjp0cnVlfQ=='})
-    data = response.json()
-    print(data)
-    embed = discord.Embed(title="Course", color=0x00ff00)
+    embed=discord.Embed(title="Course", color=0x00ff00)
     embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
-    embed.add_field(name="Course", value=course, inline=False)
-    desc=data['courseInformation']['courses'][0]['description']
-    embed.add_field(name="Description", value=desc, inline=False)
-    print(desc)
+    embed.add_field(name=course, value="Please wait while we fetch the details for "+course, inline=False)
     await ctx.send(embed=embed)
+    try:
+        response = requests.get(
+            'https://mydegree.ucdavis.edu/responsiveDashboard/api/course-link?discipline=' +
+            course.split(' ')[0]+'&number='+course.split(' ')[1]+'&',
+            headers={'Cookie': 'NAME=Parth%20Ninad%20Shah; REFRESH_TOKEN=Bearer+eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MTY4MjkxNzEiLCJpbnRlcm5hbElkIjoiOTE2ODI5MTcxIiwidXNlckNsYXNzIjoiU1RVIiwiYXBwTmFtZSI6ImRlZ3JlZXdvcmtzIiwibmFtZSI6IlNoYWgsIFBhcnRoIE5pbmFkIiwiZXhwaXJlSW5jcmVtZW50U2Vjb25kcyI6NTk5OTQwLCJleHAiOjE2NDc5MTk4MDksImFsdElkIjoicGFydGgxMjMiLCJpYXQiOjE2NDczMTk4NjksImp0aSI6ImNlNzA1N2VmLWI3ZmItNDEzMy05YTM0LTM0MDlhZDA2NzkyZSJ9.RtaH12n9SntJg24SdnbvpSeTrUrgs1z0-SBUC0MeUPA; X-AUTH-TOKEN=Bearer+eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiI5MTY4MjkxNzEiLCJpbnRlcm5hbElkIjoiOTE2ODI5MTcxIiwidXNlckNsYXNzIjoiU1RVIiwiYXBwTmFtZSI6ImRlZ3JlZXdvcmtzIiwicm9sZXMiOlsiU0RBVURJVCIsIlNEQVVEUkVWIiwiU0RXSEFUSUYiLCJOT1JFRkVSIiwiU0RXSUZISVMiLCJTRFdPUktTIiwiU0RYTUwzMyIsIlNEQVVEUlVOIiwiU0RYTUwzMSIsIlNEQVVEUERGIiwiU0RMT0tBSEQiLCJTRFdFQjMxIiwiU0RXRUIzMyIsIlNEU1RVTUUiLCJTRFdFQjM2Il0sIm5hbWUiOiJTaGFoLCBQYXJ0aCBOaW5hZCIsImRlcGFydG1lbnRzIjpbXSwiZXhwIjoxNjQ3OTIwMDA3LCJhbHRJZCI6InBhcnRoMTIzIiwiaWF0IjoxNjQ3MzIwMDY3LCJqdGkiOiI2NjdmODU1Zi01NjVlLTQ3YWMtYjYwMy0wZGQwMjMyMmI2ZTcifQ.pX4LNCbTrPoJDHWVlKcWuwo23fuHV9oPEH_puToag4E; JSESSIONID=A2F4666A9E4A939FDA4613C9F0C8EF6B; _hjSessionUser_648798=eyJpZCI6ImExMzk0ODA1LTY4MmQtNTdhMy1iMDdhLTgwMjM0NTM1YTJkOCIsImNyZWF0ZWQiOjE2Mzg1NzE4MTk0MzksImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_653936=eyJpZCI6ImUxMDcyNWY3LTdlMDgtNWY0MC1iMjRmLTQxMTE3NzdkOTE3YiIsImNyZWF0ZWQiOjE2Mzg3NzI2OTU2ODUsImV4aXN0aW5nIjp0cnVlfQ==; __utmc=162187235; _hjSessionUser_648769=eyJpZCI6IjkzOWJmYmI5LTkxMjItNTc2Yi1hM2YzLTI0ZTE1YzliZDJkOCIsImNyZWF0ZWQiOjE2Mzk2MjYxMzAwNTksImV4aXN0aW5nIjp0cnVlfQ==; _hjSessionUser_656324=eyJpZCI6ImYyYzgxYzJmLWI2ZjQtNTFiMC1hMzk0LTJhYTM3MWVhMjA0ZiIsImNyZWF0ZWQiOjE2Mzk2Mjg2OTMwNTQsImV4aXN0aW5nIjp0cnVlfQ==; nmstat=aed74523-ab5d-f360-8392-3689cadc204e; citrix_ns_id=AAE7gX3UYTszlOcCAAAAADs9GSgA5c91NSPIO5hox76dPDDtnsvKaZO-SBVZEOITOw==B4HUYQ==XbODb_NCGQLnpHv_RJ5o2oh0K7A=; citrix_ns_id_.ucdavis.edu_%2F_wat=AAAAAAWOa83Xu667afSVnlKdioV6kBJzDq_SaGolWna8d5abXybuc354qBwMBtGXOPgmQjIt4vEs_WL-P1AAR44Q0JTo&AAAAAAUEUi_GkPTPKKW5aWHCz0JMcVLaM0-KqpSt8NK5AnaTux6vH-5f3XNzidGvbxG49UaxKQJ_Vd55R9DmHm_oxkb8RZ2iQc-B_D4VeGFYxrKnKg==&AAAAAAV9ZN2kjRuOWE1I1x2qwPWGgXE1lWVeTMtJP4rJwIF4eLpbB_YTJigwGCU3Z0xp662cUYMTTruU4Zag0lMUV9lf6rtudtCIVRpifwEsWa4v5g==&; SAP_CASAUTH=4D99A2C728D939B6A652E6CB7859C2BD43639A19F6CFCDD5FA2C60ADA08C02A1550FF5D61587732DEE15F2055B04E4513605DA27A66E9B974A4ECAF417DF27EA180B99A3164CF05F1BA3EBCA8FA0BAD92A2D7BA3EDEEDB3043FC6564032A3AF2; __utmz=162187235.1646345630.14.7.utmcsr=cas.ucdavis.edu|utmccn=(referral)|utmcmd=referral|utmcct=/; _fbp=fb.1.1646348587255.953116442; __utma=162187235.1828723012.1638322952.1646345630.1646349181.15; _ga=GA1.2.1828723012.1638322952; _ga_YQ20RZQYKR=GS1.1.1646354327.2.0.1646354327.60; _gid=GA1.2.273393137.1647237034; _hjSession_648798=eyJpZCI6IjliYTc2YTlhLTM1NzUtNGIxNi04N2Q0LTZiNmZlNjRlYzAzZCIsImNyZWF0ZWQiOjE2NDczMTk0MTg1NjIsImluU2FtcGxlIjp0cnVlfQ==; MYUCDAVIS_LANDING_IMAGE=bikesandtrees%2Ejpg%2Cblossoms%2Ejpg%2Cbunitransblur%2Ejpg%2Cccinterior%2Ejpg%2Ccpool%2Ejpg%2Ccwolfskill2%2Ejpg%2Cdeathblossom%2Ejpg%2Cdkayaks%2Ejpg%2Cdorms2%2Ejpg%2Cegret%2Ejpg%2Cflag2%2Ejpg%2CginsengMrak%2Ejpg%2Cgoags%2Ejpg%2Clibrary%2Ejpg%2Corchard%2Ejpg%2Cpallette%2Ejpg%2Cpcircle%2Ejpg%2Cplantsciences%2Ejpg%2Csciencewall%2Ejpg%2Csilo2%2Ejpg%2Cspokes%2Ejpg%2CstudentFarmPoppy%2EJPG%2Cterceroquad%2Ejpg; _gcl_au=1.1.1866842315.1647319836; _hjSession_653936=eyJpZCI6IjA4ZWMyNGQxLWZlYjItNDMxYS1hNTU0LWE1N2ZiYjg4Yjg0MCIsImNyZWF0ZWQiOjE2NDczMTk4MzYyNDksImluU2FtcGxlIjp0cnVlfQ=='})
+        data = response.json()
+        desc = data['courseInformation']['courses'][0]['description']
+        embed = discord.Embed(title="Course", color=0x00ff00)
+        embed.set_author(name=ctx.author.name, icon_url=ctx.author.avatar_url)
+        embed.add_field(name="Course", value=course, inline=False)
+        embed.add_field(name="Description", value=desc, inline=False)
+        print(desc)
+        await ctx.send(embed=embed)
+    except:
+        await ctx.send("Course not found")
+
 
 @bot.event
 async def on_message(message):
